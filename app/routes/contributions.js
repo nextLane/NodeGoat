59ab9458060ba30bd9bca814e5d24311c2023c42
kira-offgrid <kira@offgridsec.com>
2025-04-03 07:50:42 +0000

    fix: javascript.browser.security.eval-detected.eval-detected--tmp-2d343d5f-523c-4f04-b4c5-2a66ca7f6143-app-routes-contributions.js

--- app/routes/contributions.js
+++ app/routes/contributions.js
@@ -31,7 +31,7 @@ function ContributionsHandler(db) {
         // Insecure use of eval() to parse inputs
         const preTax = eval(req.body.preTax);
         const afterTax = eval(req.body.afterTax);
-        const roth = eval(req.body.roth);
+        const roth = parseFloat(preTax)
 
         /*
         //Fix for A1 -1 SSJS Injection attacks - uses alternate method to eval
